6d0cb55f857ac239ec1e36936c83542198d542c9
Serhii Kupriienko <skupriienko@anaconda.com>
2022-09-05 14:52:41 +0000

    Add pytest to run_test.py

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,4 +1,30 @@
+import os
+import pkgutil
+import platform
+import sys
+
+import pytest
 from prophet import Prophet
 
-m = Prophet()
-print(f'Using backend: {m.stan_backend.get_type()}')
+
+def go():
+    py_major = sys.version_info[0]
+    py_impl = platform.python_implementation().lower()
+    machine = platform.machine().lower()
+
+    print("Python implementation:", py_impl)
+    print("              Machine:", machine)
+
+    m = Prophet()
+    print(f'Using backend: {m.stan_backend.get_type()}')
+
+    loader = pkgutil.get_loader("prophet.tests")
+    pytest_args = [os.path.dirname(loader.path), "-vv"]
+    print("Final pytest args:", pytest_args)
+
+    # actually run the tests
+    sys.exit(pytest.main(pytest_args))
+
+
+if __name__ == "__main__":
+    go()
